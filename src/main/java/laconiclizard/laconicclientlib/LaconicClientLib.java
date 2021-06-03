63a67e55d2c158fa07e05c4ead15a38b4cda9b84
LaconicLizard <laconiclizard976@protonmail.com>
2021-06-03 10:59:51 -0700

    Initial commit.

---
+++ src/main/java/laconiclizard/laconicclientlib/LaconicClientLib.java
@@ -0,0 +1,8 @@
+package laconiclizard.laconicclientlib;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LaconicClientLib implements ModInitializer {
+    @Override public void onInitialize() {
+    }
+}
---
+++ src/main/java/laconiclizard/laconicclientlib/client/LaconicClientLibClient.java
@@ -0,0 +1,13 @@
+package laconiclizard.laconicclientlib.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT) public class LaconicClientLibClient implements ClientModInitializer {
+
+    @Override public void onInitializeClient() {
+
+    }
+
+}
