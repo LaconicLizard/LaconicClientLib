dd1be34e72275beb989e09bed9f0d657cd7b46b7
LaconicLizard <laconiclizard976@protonmail.com>
2021-06-08 10:49:19 -0700

    Added more constructors to ClientChatPrinter, added ability to optionally style the prefix the same as the message.

--- src/main/java/laconiclizard/laconicclientlib/ClientChatPrinter.java
+++ src/main/java/laconiclizard/laconicclientlib/ClientChatPrinter.java
@@ -8,17 +8,31 @@ import net.minecraft.util.Formatting;
 public class ClientChatPrinter {
 
     public Text prefix;
+    public boolean stylePrefix;
     public Style infoStyle = Style.EMPTY.withColor(Formatting.WHITE),
             warnStyle = Style.EMPTY.withColor(Formatting.YELLOW),
             errorStyle = Style.EMPTY.withColor(Formatting.RED),
             debugStyle = Style.EMPTY.withColor(Formatting.BLUE);
 
     public ClientChatPrinter() {
-        this(Text.of(""));
+        this("");
     }
 
     public ClientChatPrinter(Text prefix) {
+        this(prefix, false);
+    }
+
+    public ClientChatPrinter(Text prefix, boolean stylePrefix) {
         this.prefix = prefix;
+        this.stylePrefix = stylePrefix;
+    }
+
+    public ClientChatPrinter(String prefix) {
+        this(Text.of(prefix));
+    }
+
+    public ClientChatPrinter(String prefix, boolean stylePrefix) {
+        this(Text.of(prefix), stylePrefix);
     }
 
     /**
@@ -36,14 +50,19 @@ public class ClientChatPrinter {
     /**
      * Print msg with this ClientChatPrinter's prefix, but otherwise unmodified.
      *
-     * @param msg message to print
+     * @param msg      message to print
+     * @param msgStyle the style of the message; used to style the prefix if .stylePrefix is true
      */
-    public void printPrefixed(Text msg) {
-        printRaw(prefix.copy().append(msg));
+    public void printPrefixed(Text msg, Style msgStyle) {
+        MutableText p = prefix.copy();
+        if (stylePrefix) {
+            p.setStyle(msgStyle);
+        }
+        printRaw(p.append(msg));
     }
 
-    public void printPrefixed(String msg) {
-        printPrefixed(Text.of(msg));
+    public void printPrefixed(String msg, Style msgStyle) {
+        printPrefixed(Text.of(msg), msgStyle);
     }
 
     /**
@@ -52,7 +71,7 @@ public class ClientChatPrinter {
      * @param msg message to print
      */
     public void info(Text msg) {
-        printPrefixed(msg.copy().setStyle(infoStyle));
+        printPrefixed(msg.copy().setStyle(infoStyle), infoStyle);
     }
 
     public void info(String msg) {
@@ -65,7 +84,7 @@ public class ClientChatPrinter {
      * @param msg message to print
      */
     public void warn(Text msg) {
-        printPrefixed(msg.copy().setStyle(warnStyle));
+        printPrefixed(msg.copy().setStyle(warnStyle), warnStyle);
     }
 
     public void warn(String msg) {
@@ -78,7 +97,7 @@ public class ClientChatPrinter {
      * @param msg message to print
      */
     public void error(Text msg) {
-        printPrefixed(msg.copy().setStyle(errorStyle));
+        printPrefixed(msg.copy().setStyle(errorStyle), errorStyle);
     }
 
     public void error(String msg) {
@@ -91,7 +110,7 @@ public class ClientChatPrinter {
      * @param msg message to print
      */
     public void debug(Text msg) {
-        printPrefixed(msg.copy().setStyle(debugStyle));
+        printPrefixed(msg.copy().setStyle(debugStyle), debugStyle);
     }
 
     public void debug(String msg) {
